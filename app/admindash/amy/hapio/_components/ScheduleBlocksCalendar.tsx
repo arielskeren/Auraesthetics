32ea1cb521243db2245e6dce8a465c9cc9662180
arielskeren <arielskeren@gmail.com>
2025-11-15 13:35:31 -0500

    Update ScheduleBlocksCalendar.tsx

--- app/admindash/amy/hapio/_components/ScheduleBlocksCalendar.tsx
+++ app/admindash/amy/hapio/_components/ScheduleBlocksCalendar.tsx
@@ -720,6 +720,7 @@ export default function ScheduleBlocksCalendar({
                   </div>
                 </div>
               )}
+            </div>
           </div>
         )}
       </div>
